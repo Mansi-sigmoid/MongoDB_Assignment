83ef84b75d3cab3b9ec916c54ea0d2c3fc273736
Mansi-sigmoid <98111326+Mansi-sigmoid@users.noreply.github.com>
2022-03-04 17:14:36 +0530

    Add files via upload

---
+++ T1_movies.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+import json
+from bson import ObjectId
+
+try:
+    connection = MongoClient('localhost', 27017)
+except:
+    print("Error in Connect")
+
+db = connection['mflix']
+
+collection = db['movies']
+
+upd_item_list = []
+
+with open('sample_mflix/movies.json') as f:
+    for obj in f:
+        if obj:
+            upd_my_dict = json.loads(obj)
+            upd_my_dict["_id"] = ObjectId(upd_my_dict["_id"]["$oid"])
+            upd_item_list.append(upd_my_dict)
+
+collection.insert_many(upd_item_list)
+
+
+
+
+
